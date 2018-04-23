d6db73ed1fa1d0ffba1038caf8feb9361b24a1cf
Tatvam <td14@iitbbs.ac.in>
2018-04-24 01:20:14 +0530

    More are Coming

---
+++ Data Structures/Disjoint_Set.cpp
@@ -0,0 +1,91 @@
+/**************************************************
+*    NAME:           TATVAM DADHEECH(td_1417)     *
+*    INSTITUITION:   IIT BHUBANESWAR              *
+**************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<long long> vl;
+typedef vector<string> vs;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define vzero fill(v.begin(), v.end(), 0);
+#define re(i, n) for(int i = 0; i < n; i++)
+#define test int t; cin >> t; while(t--)
+#define rep(i,a,b) for(int i = (a); i <= (b); i++)
+#define ren(i,a,b) for(int i = (a); i >= (b); i--)
+#define turbo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define MOD 1e9+7
+#define PI 3.1415926535897932384626433832795
+#define MAX 100001
+#define INF (1<<20)
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+struct Node{
+    int parent;
+    int rank;
+};
+
+Node DSU[1001];
+
+void makeset(int i){
+    DSU[i].parent = i;
+    DSU[i].rank = 0;
+}
+
+int root(int x){
+    while(DSU[x].parent != x){
+        DSU[x].parent = DSU[DSU[x].parent].parent;
+        x = DSU[x].parent;
+    }
+    return x;
+}
+bool issame(int x,int y){
+    if(root(x)==root(y)){return true;}
+    return false;
+}
+int main(){
+    int q;
+    cin>>q;
+    re(i,q){
+        int n;
+        cin>>n;
+        if(n == 0){
+            int t;
+            cin>>t;
+            makeset(t);
+        }
+        else if(n == 1){
+            int a,b;
+            cin>>a>>b;
+            int x = root(a);
+            int y = root(b);
+            if(x == y)continue;
+            else if(DSU[x].rank > DSU[y].rank){
+                DSU[y].parent = x;
+            }
+            else if(DSU[x].rank < DSU[y].rank){
+                DSU[x].parent = y;
+            }
+            else{
+                DSU[x].parent = y;
+                DSU[y].rank++;
+            }
+        }
+        else{
+            int x,y;
+            cin>>x>>y;
+            bool ans = issame(x,y);
+            if(ans){
+                cout<<"Yes"<<endl;
+            }
+            else{
+                cout<<"No"<<endl;
+            }
+        }
+    }
+}
