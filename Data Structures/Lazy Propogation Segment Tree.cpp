06efd3aeef4faa0752339b9827c3cb4478cb2901
Tatvam <td14@iitbbs.ac.in>
2018-06-07 00:43:49 +0530

    More Are Coming4

---
+++ Data Structures/Lazy Propogation Segment Tree.cpp
@@ -0,0 +1,80 @@
+/*
+ the update operation used is adding a given value to each element in the range [l,r]
+*/
+#include <iostream>
+#include <climits>
+using namespace std;
+int lazy[1000]={0};
+void buildTree(int *tree,int *a,int start,int end,int node){
+	if(start==end)tree[node]=a[start];
+	else if(start<end){
+		int mid=(start+end)/2,left=2*node+1,right=left+1;
+		buildTree(tree,a,start,mid,left);
+		buildTree(tree,a,mid+1,end,right);
+		tree[node]=min(tree[left],tree[right]);
+	}
+}
+void update(int *tree,int start,int end,int l,int r,int dif,int node){
+	if(lazy[node]!=0){
+		// there are pending updates
+		tree[node]+=lazy[node];
+		if(start!=end){
+			lazy[2*node+1]+=lazy[node];
+			lazy[2*node+2]+=lazy[node];
+		}
+		lazy[node]=0;
+	}
+	if(start> r or end<l)return;
+	else if(start>=l and end<=r){
+		tree[node]+=dif;	// make the update for the current range and keep them pending for the child nodes
+		if(start!=end){
+			lazy[2*node+1]+=dif;
+			lazy[2*node+2]+=dif;
+		}
+		return;
+	}
+	int mid=(start+end)/2;
+	update(tree,start,mid,l,r,dif,2*node+1);
+	update(tree,mid+1,end,l,r,dif,2*node+2);
+	tree[node]=min(tree[2*node+1],tree[2*node+2]);
+}
+int queryMin(int *tree,int start,int end,int l,int r,int node){
+	if(lazy[node]!=0){
+		// if there are updates pending, apply them before making the queries
+		tree[node]+=lazy[node];
+		if(start!=end){
+			lazy[2*node+1]=lazy[node];
+			lazy[2*node+2]=lazy[node];
+		}
+		lazy[node]=0;
+	}
+	if(start>r or end<l)return INT_MAX-1;
+	else if(start>=l and end<=r)return tree[node];
+	else{
+		int mid=(start+end)/2,left=2*node+1,right=left+1;
+		return min(queryMin(tree,start,mid,l,r,left),queryMin(tree,mid+1,end,l,r,right));
+	}
+}
+int main(){
+	int n,i,tree_size;
+	cin>>n;
+	int a[n];
+	tree_size=2*n;
+	int tree[tree_size];
+	for(i=0;i<n;i++)cin>>a[i];
+	buildTree(tree,a,0,n-1,0);
+	int n_ops;	// number of operations to be performed
+	cin>>n_ops;
+	while(n_ops--){
+		int x,l,r;
+		cin>>x>>l>>r;
+		if(x==0)
+		cout<<queryMin(tree,0,n-1,l-1,r-1,0)<<endl;
+		else
+		{
+			int dif;
+			cin>>dif;
+			update(tree,0,n-1,l-1,r-1,dif,0);
+		}
+	}
+}
---
+++ Data Structures/Segment Tree.cpp
@@ -0,0 +1,80 @@
+/**************************************************
+*    NAME:           TATVAM DADHEECH(td_1417)     *
+*    INSTITUITION:   IIT BHUBANESWAR              *
+**************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<long long> vl;
+typedef vector<string> vs;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+#define vzero fill(v.begin(), v.end(), 0);
+#define re(i, n) for(int i = 0; i < n; i++)
+#define test int t; cin >> t; while(t--)
+#define rep(i,a,b) for(int i = (a); i <= (b); i++)
+#define ren(i,a,b) for(int i = (a); i >= (b); i--)
+#define turbo ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define MAX 100001
+#define INF (1<<20)
+#define si(x)\tscanf("%d",&x)
+#define sl(x)\tscanf("%lld",&x)
+#define ss(s)\tscanf("%s",s)
+#define pi(x)\tprintf("%d\n",x)
+#define pl(x)\tprintf("%lld\n",x)
+#define ps(s)\tprintf("%s\n",s)
+int constructSeg(vector<int> a,vector<int> &s,int ss,int se,int si){
+    if(ss == se){
+        s[si]=a[ss];
+        return s[si];
+    }
+
+    int mid = (ss+se)/2;
+
+    s[si]=min(constructSeg(a,s,ss,mid,2*si+1),constructSeg(a,s,mid+1,se,2*si+2));
+
+    return s[si];
+}
+int RMQUtil(vector<int> s,int qs,int qe,int ss,int se,int index){
+    if(qe>=se && qs<=ss){
+        return s[index];
+    }
+    if(qe<ss || qs>se ){
+        return INT_MAX;
+    }
+     int mid = (ss+se)/2;
+    return min(RMQUtil(s, qs, qe,ss, mid,  2*index+1),
+                  RMQUtil(s, qs, qe,mid+1, se, 2*index+2));
+}
+int RMQ(vector<int> s,int qs,int qe,int n){
+
+    if(qs>qe || qs<0 || qe>n-1){
+        return 0;
+    }
+   return  RMQUtil(s,qs,qe,0,n-1,0);
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    re(i,n){
+        cin>>a[i];
+    }
+    int m = 2*pow(2,(int)ceil(log2(n)))-1;
+    vector<int> s(m);
+    constructSeg(a,s,0,n-1,0);
+  //  re(i,m){
+    //    cout<<s[i]<<" ";
+   // }
+    cout<<endl;
+    int q;
+    cin>>q;
+    re(i,q){
+        int qs,qe;
+        cin>>qs>>qe;
+       cout<< RMQ(s,qs,qe,n)<<endl;;
+    }
+
+}
