f62d8176f5daa801474f5afdb92791bb1d98f46c
Tatvam <td14@iitbbs.ac.in>
2018-06-28 23:40:04 +0530

    More Are Coming

--- Graph Algorithms/Articulation Point.cpp
+++ Graph Algorithms/Articulation Point.cpp
@@ -25,110 +25,62 @@ typedef pair<ll,ll> pll;
 #define pi(x) printf("%d\n",x)
 #define pl(x) printf("%lld\n",x)
 #define ps(s) printf("%s\n",s)
-// DFS
-/*
-    vector<int> adj[100000];
-    vector<bool> visited(100000);
- dfs(int s,vector<bool> &visited)
+
+vector<int> adj[100000];
+vector<bool> visited(100000,false);
+vector<int> disc(100000);
+vector<int> low(100000);
+vector<int> parent(100000,-1);
+vector<bool> arti(100000,false);
+int tim = 0;
+void dfs(int u)
 {
-    visited[s]=true;
-    for(int i=0;i<adj[s].size();i++)
+    int children = 0;
+    disc[u]=low[u]=tim++;
+    visited[u]=true;
+    for(int i=0;i<adj[u].size();i++)
     {
-        if(visited[adj[s][i]]==false)
-        dfs(adj[s][i],visited);
-    }
-}
-*/
+        int v = adj[u][i];
+        if(visited[v]==false){
+            children ++;
+            parent[v]=u;
 
-// BFS
-/*
-    void bfs(int s,vector<int> &level)
-{
-    queue<int> q;
-    q.push(s);
-    level[s]=1;
-    visited[s]=true;
-    while(!q.empty())
-     {
-       int p=q.front();
-       q.pop();
-       for(int i=0;i<adj[p].size();i++)
-        {
-          if(visited[adj[p][i]]==false)
-         {
+            dfs(adj[u][i]);
 
-              q.push(adj[p][i]);
-              level[adj[p][i]]=level[p]+1;
-              visited[adj[p][i]]=true;
-          }
+            low[u]=min(low[u],low[v]);
+
+            if(parent[u]==-1 && children>1){
+                arti[u]=true;
+            }
+            if(low[v]>=disc[u] && parent[u]!=-1){
+                arti[u]=true;
+            }
         }
-      }
-}
-*/
-// Binary Search
-/*
-int Binarysearch(vector<int> a,int n,int key)
-{
-    int low,high,mid;
-    low=0;
-    high=n-1;
-    
-    while(low<=high)
-    {
-        mid=(low+high)/2;
-        if(key==a[mid])
-            return(mid);
-        else
-            if(key>a[mid])
-                low=mid+1;
-            else
-                high=mid-1;
+        else if(parent[u]!=v){
+            low[u]=min(low[u],disc[v]);
+        }
+        
     }
-    
-    return -1;
-}
-*/
-// DSU
-/*
-struct Node{
-    int parent;
-    int rank;
-};
-
-Node DSU[1001];
-void union(){
-    int a,b;
-            cin>>a>>b;
-            int x = root(a);
-            int y = root(b);
-            if(x == y)continue;
-            else if(DSU[x].rank > DSU[y].rank){
-                DSU[y].parent = x;
-            }
-            else if(DSU[x].rank < DSU[y].rank){
-                DSU[x].parent = y;
-            }
-            else{
-                DSU[x].parent = y;
-                DSU[y].rank++;
-            }
-}
-void makeset(int i){
-    DSU[i].parent = i;
-    DSU[i].rank = 0;
 }
 
-int root(int x){
-    while(DSU[x].parent != x){
-        DSU[x].parent = DSU[DSU[x].parent].parent;
-        x = DSU[x].parent;
-    }
-    return x;
-}
-bool issame(int x,int y){
-    if(root(x)==root(y)){return true;}
-    return false;
-}
-*/
+
 int main(){
+    int nodes,edges;
+    cin>>nodes>>edges;
+    re(i,edges){
+        int x,y;
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);    
+      }
+      re(i,nodes){
+          if(visited[i]==false){
+              dfs(i);
+          }
+      }
+      re(i,nodes){
+          if(arti[i]){
+              cout<<i<<" "<<"Destroyer"<<endl;
+          }
+      }
 }
